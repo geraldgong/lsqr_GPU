efbcdc2122475b19677b81ee701571f793c04053
geraldgong <garygong0430@gmail.com>
2015-04-04 18:44:43 +0200

    compile .cu kernel

--- runFunction.m
+++ runFunction.m
@@ -1,8 +1,9 @@
 clear all; close all; clc
-system('nvcc -c lsqr_Cuda.cu  -lcublas -ccbin "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin" -I"C:\Program Files\MATLAB\R2015a\extern\include" ');
-mex lsqr_Cuda.cpp lsqr_Cuda.obj -lcudart -lcublas -L"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v6.5\lib\x64" ...
- -v -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v6.5\include";
+
+% system('nvcc -c test.cu  -lcudart -lcublas -ccbin "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin" -I"C:\Program Files\MATLAB\R2015a\extern\include" ');
+% mex test.obj -L"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v5.5\lib\x64" ...
+%  -v -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v5.5\include" -lcudart -lcublas;
 % 
-%  system('nvcc -c lsqr_version1.cu -lcublas -ccbin "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin" -I"C:\Program Files\MATLAB\R2015a\extern\include" ');
-%  mex lsqr_version1.cpp lsqr_version1.obj  -lcudart -lcublas -L"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v6.5\lib\x64" ...
-%       -v -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v6.5\include";
+system('nvcc -c lsqr_sv_Cuda.cu  -lcudart -lcublas -ccbin "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin" -I"C:\Program Files\MATLAB\R2015a\extern\include" ');
+mex lsqr_sv_Cuda.obj -L"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v5.5\lib\x64" ...
+ -v -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v5.5\include" -lcudart -lcublas;

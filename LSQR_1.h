c4692b0e25ef1e1139a21a29d9794fcf614634b3
geraldgong <garygong0430@gmail.com>
2015-04-14 00:02:47 +0200

    delete old versions

--- LSQR_1.h
+++
@@ -1,6 +0,0 @@
-#ifndef __LSQR_1__
-#define __LSQR_1__
-// extern void test(float *A, float *As, float *b, float *Vector, int *posx, int *posy, int n, int nz, int w, int h);
-extern void LSQR_1(float *A, float *b, float *Vector, int *posx, int *posy, int n, int nz, int w, int h);
-// extern indicates that our function is implemented in some other file.
-#endif // __LSQR_1__
